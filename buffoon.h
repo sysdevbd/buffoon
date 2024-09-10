c6cf24b273ad453ccc1c49ae952cc78378051595
shiponcs <abdulmatincuetcse17@gmail.com>
2024-09-10 09:55:10 +0600

    Add library header file Signed-off-by: shiponcs <abdulmatincuetcse17@gmail.com>

---
+++ buffoon.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+
+namespace Buffoon {
+  class Buffoon {
+  public:
+      /**
+       * method to find the value keyed by "payload" from JSON-like string
+       * @param json_like_string, key(unquoted key)
+       * @return payload (null string if "payload" key is not found)
+       */
+      static std::string findValueByKey(const std::string& json_like_string, const std::string& key);
+      /**
+       * method to find the payload value from JSON-like string
+       * @param payloadValue
+       * @return username
+       */
+      static std::string findUsername(const std::string& payload_value);
+  };
+} // namespace Buffoon
